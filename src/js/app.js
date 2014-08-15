f1a3946ce09c4946fc5179fffdcc5d1798e58818
oncletom <hi@oncletom.io>
2014-08-15 13:00:11 +0100

    Fav toggling and react-classset contribution.

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,11 @@ var React = module.exports.React = require('react');
 var appContainerElement = document.getElementById('app-container');
 
 /*
+ React Setup
+ */
+React.initializeTouchEvents(true);
+
+/*
  Construct the App skeleton
  */
 React.renderComponent(MainView({ items: [] }), appContainerElement);
@@ -14,7 +19,8 @@ React.renderComponent(MainView({ items: [] }), appContainerElement);
 /*
  Gets data asynchronously
  */
-getData('https://s3-eu-west-1.amazonaws.com/irfs-application/data.json', onDataSuccess, onDataError);
+getData('data.json', onDataSuccess, onDataError);
+//getData('https://s3-eu-west-1.amazonaws.com/irfs-application/data.json', onDataSuccess, onDataError);
 
 /*
  Callbacks
--- src/js/components/item.jsx
+++ src/js/components/item.jsx
@@ -3,24 +3,73 @@
 'use strict';
 
 var React = require('react');
+var cx = require('react-classset');
+
+var Duration = require('./duration.jsx');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return { selected: false, favourited: false };
+  },
+
   render: function(){
     var data = this.props.data;
 
     return (
-      <article className="item">
+      <article className={this.getClassNames()} onClick={this.handleClick}>
         <header className="item__cover" data-background={data.image}></header>
 
         <h2>{data.name}</h2>
 
         <footer>
           <ul className="item-metadata">
-            <li class="item-metadata__source">{data.source}</li>
-            <li class="item-metadata__duration">{data.duration}</li>
+            <li className="item-metadata__source">{data.source}</li>
+            <li className="item-metadata__duration"><Duration duration={data.duration} /></li>
+          </ul>
+
+          <ul className="item-actions">
+            <li className="item-actions__playback">
+              <a href="#play" onClick={this.handleClick}>
+                {data.duration && 'Play'}
+              </a>
+            </li>
+            <li className="item-actions__favourites">
+              <a href="#favourite" onClick={this.toggleFavourite}>
+                {!this.state.favourited ? 'Add to' : 'Remove from'} favourites
+              </a>
+            </li>
           </ul>
         </footer>
       </article>
       );
+  },
+
+  /*
+   Events
+   */
+  handleClick: function(event){
+    event.preventDefault();
+
+    this.setState({ selected: !this.state.selected, favourited: this.state.favourited });
+  },
+
+  toggleFavourite: function toggleFavourite(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.setState({ selected: this.state.selected, favourited: !this.state.favourited });
+  },
+
+  /*
+   UI Helpers
+   */
+
+  getClassNames: function getClassNames(){
+    return cx(
+      'item',
+      'item--' + (this.props.data.type || 'unknown'),
+      this.state.favourited && 'item--favourited',
+      this.state.selected && 'item--selected'
+    );
   }
 });
